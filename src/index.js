eb65505ceab6595fd1d00feb6f72081cc37cf175
Rishav Kumar <rishavkr000@gmail.com>
2022-05-04 08:40:15 +0530

    Final Commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://group15_project:EDHBqxqKYJaki5EJ@cluster0.i9alz.mongodb.net/RishavDB", {
+mongoose.connect("mongodb+srv://group15_project:EDHBqxqKYJaki5EJ@cluster0.i9alz.mongodb.net/Project", {
 
     useNewUrlParser: true
     
