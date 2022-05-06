265699d64887e67aea183c1a8ac8554377f63ac4
krish2592 <sachink9225s@gmail.com>
2022-05-06 14:46:40 +0530

    Added functionality of  collegeName in createIntern

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -8,9 +8,9 @@ let createIntern = async function (req, res) {
         let data = req.body;
         if (!isValidRequestBody(data)) return res.status(400).send({ status: false, msg: "No user input" })
 
-        let { name, email, mobile, collegeId } = data;
+        let { name, email, mobile, collegeName } = data;
         if (!isValid(name)) return res.status(400).send({ status: false, msg: "Name is required" })
-        if(!isValidFullName(name)) return res.status(400).send({ status: false, msg: "Name only contain alphabet and space" })
+        if (!isValidFullName(name)) return res.status(400).send({ status: false, msg: "Name only contain alphabet and space" })
         if (!isValid(email)) return res.status(400).send({ status: false, msg: "Email is required" })
         if (!isValidEmail(email)) return res.status(400).send({ status: false, msg: `${email} is not a valid email` })
 
@@ -20,10 +20,13 @@ let createIntern = async function (req, res) {
         if (!isValidMobile(mobile)) return res.status(400).send({ status: false, msg: `${mobile} is not a valid mobile number` })
         const isUniqueMobile = await internModel.findOne({ mobile: mobile })
         if (isUniqueMobile) return res.status(400).send({ status: false, message: `This mobile number ${mobile} is already exist` })
-        if (!isValid(collegeId)) return res.status(400).send({ status: false, msg: "College Id is required" })
-        if (!isValidObjectId(collegeId)) return res.status(400).send({ status: false, msg: `${collegeId} is not a valid object id` })
-
-        let internData = { name,email, mobile, collegeId }
+        if (!isValid(collegeName)) return res.status(400).send({ status: false, msg: "College Name is required" })
+        let collegeData = await collegeModel.findOne({ name: collegeName });
+        if (Object.keys(collegeData).length===0) return res.status(400).send({ status: false, msg: "College data not found!" })
+        let { _id } = collegeData;
+        if (!isValidObjectId(_id)) return res.status(400).send({ status: false, msg: `${_id} is not a valid object id` })
+        let collegeId = _id;
+        let internData = { name, email, mobile, collegeId };
         const saveData = await internModel.create(internData);
 
         return res.status(201).send({ status: true, msg: "Intern data saved sucessfully", data: saveData })
@@ -44,10 +47,10 @@ const getIntern = async function (req, res) {
         if (Array.isArray(getCollege) && getCollege.length === 0) return res.status(400).send({ status: false, msg: "College not found!" })
         let [{ _id, name, fullName, logoLink }] = getCollege;
         if (!isValidObjectId(_id)) return res.status(400).send({ status: false, msg: "College id is not a valid object id" })
-        const getIntern = await internModel.find({ collegeId: _id}).select({_id:1,name:1,email:1,mobile:1})
+        const getIntern = await internModel.find({ collegeId: _id }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
         if (Array.isArray(getIntern) && getIntern.length === 0) return res.status(400).send({ status: false, msg: "Intern data not found!" })
 
-        return res.status(200).send({status:true, data: { name: name, fullName: fullName, logoLink: logoLink, interests: getIntern } })
+        return res.status(200).send({ status: true, data: { name: name, fullName: fullName, logoLink: logoLink, interests: getIntern } })
     }
     catch (err) {
         return res.status(500).send({ status: false, error: err.message })
